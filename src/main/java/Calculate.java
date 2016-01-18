73ba468e96104bc46da80ac3b9732d05d89f5dc5
kate <kateryna.maksimenko@gmail.com>
2016-01-18 20:59:27 +0200

    changes

--- src/main/java/Calculate.java
+++ src/main/java/Calculate.java
@@ -9,8 +9,16 @@ public class Calculate {
     }
 
     public int add(int a, int b) {
+
         return a + b;
     }
+
+    public int multiply(int a, int b) {
+
+        return a * b;
+    }
+
+
 
     //TODO
     //add methods for multiply, division, subtraction, square root, x^2
